a6e28261074cf17badfd71897bc2c85a0c47d29a
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-08-21 22:58:35 +0700

    feat: enhance dashboard with news and services statistics; add DashboardManagement component for better data presentation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NewsSection;
+use App\Models\ServiceSection;
+use Carbon\Carbon;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return inertia('dashboard/page');
+        $total_news_on_this_month = NewsSection::whereMonth('date', Carbon::now()->month)
+            ->whereYear('date', Carbon::now()->year)->count();
+
+        $total_news_on_this_month_formatted = number_format($total_news_on_this_month, thousands_separator: '.');
+
+        $news_of_all_time_formatted = number_format(NewsSection::count());
+
+        $services_of_all_time_formatted = number_format(ServiceSection::count());
+
+        $current_date = intval(Carbon::now()->format('d'));
+
+        $average_news_on_this_month_formatted = number_format(
+            $total_news_on_this_month / $current_date,
+            thousands_separator: '.'
+        );
+
+        return inertia('dashboard/page', compact(
+            'total_news_on_this_month_formatted',
+            'news_of_all_time_formatted',
+            'services_of_all_time_formatted',
+            'average_news_on_this_month_formatted'
+        )
+        );
     }
 }
--- resources/js/pages/dashboard/page.tsx
+++ resources/js/pages/dashboard/page.tsx
@@ -1,8 +1,33 @@
+import { Head } from "@inertiajs/react";
 import DashboardLayout from "../layouts/dashboard-layout"
+import DashboardManagement from "./_components/DashboardManagement";
 
-export default function Page() {
+type Props = {
+    total_news_on_this_month_formatted: number
+    news_of_all_time_formatted: number
+    services_of_all_time_formatted: number
+    average_news_on_this_month_formatted: number
+}
+
+export default function Page(props: Props) {
+    const {
+        total_news_on_this_month_formatted,
+        news_of_all_time_formatted,
+        services_of_all_time_formatted,
+        average_news_on_this_month_formatted
+    } = props;
+
     return (
-        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <>
+            <Head title="Dashboard" />
+            <h1 className="text-2xl font-semibold">Dashboard</h1>
+            <DashboardManagement
+                total_news_on_this_month_formatted={total_news_on_this_month_formatted}
+                news_of_all_time_formatted={news_of_all_time_formatted}
+                services_of_all_time_formatted={services_of_all_time_formatted}
+                average_news_on_this_month_formatted={average_news_on_this_month_formatted}
+            />
+        </>
     )
 }
 
